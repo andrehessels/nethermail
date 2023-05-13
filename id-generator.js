08128e1c569dc88738197e994fa9660138d98b39
André <57950830+andrehessels@users.noreply.github.com>
2023-05-13 15:18:14 +0200

    Update id-generator.js

--- id-generator.js
+++ id-generator.js
@@ -1,13 +1,13 @@
 function generateUniqueID() {
   if (typeof(Storage) !== "undefined") {
       if (localStorage.uniqueID) {
-          return localStorage.uniqueID; // Teruggeven van de bestaande ID
+          return localStorage.uniqueID; 
       } else {
           alert("Welcome to NetherMail. We generate a random ID and store it in your browser's local storage. You can renew your ID at any time, as much times as you want. If you switch browsers and/or devices or go into icognito mode, a new ID will get generated.\nYou can share this ID with those who want to send you an encrypted message. Messages are encrypted with AES and using a custom passcode.")
           var timestamp = Date.now();
           var uniqueID = Math.random().toString(36).substr(2, 9) + String(timestamp).substr(10, 14);
-          localStorage.uniqueID = uniqueID; // Opslaan van het nieuwe ID in de lokale opslag
-          return uniqueID; // Teruggeven van het nieuwe ID
+          localStorage.uniqueID = uniqueID; 
+          return uniqueID;
       }
   } else {
       console.error("Lokale opslag wordt niet ondersteund in deze browser.");
